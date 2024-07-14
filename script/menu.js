962f0bd0e376bc0d5bacbc28d4357650ad6fce19
Diego Quijano <106574144+DifKiG@users.noreply.github.com>
2024-07-13 23:46:51 -0500

    optimizando

--- script/menu.js
+++ script/menu.js
@@ -1,40 +1,37 @@
 //Es un arreglo de objetos tiene info de menu y sus items
 const menuData = [
-    {
-        title: "De Lunes a Jueves",
-        image: "img/arepa_icono.png",
-        items: [
-            {sabor: "Cafe", precio: "$ 1.000 pesos" },
-            {sabor: "Cafe en leche", precio: "$ 2.000 pesos" },
-            {sabor: "Arepa sin asar", precio: "$ 1.000 pesos" },
-            { sabor: "Arepa solas", precio: "$1.500 pesos" },
-            { sabor: "Arepa con Queso Lonchis", precio: "$3.000 pesos" },
-            { sabor: "Arepa con Queso Cuajada", precio: "$3.000 pesos" },
-            { sabor: "Arepa con jamon y queso", precio: "$3.500 pesos" },
-            { sabor: "Arepa con chorizo", precio: "$6.000 pesos" },
-            { sabor: "Chorizo solo", precio: "$5.500 pesos" }
-        ]
-    },
-    {
-        title: "Viernes, Domingos y Festivos",
-        image: "img/arepa_icono.png",
-        items: [
-            {sabor: "Cafe", precio: "$ 1.000 pesos" },
-            {sabor: "Cafe en leche", precio: "$ 2.000 pesos" },
-            { sabor: "Arepa sin asar", precio: "$1.000 pesos" },
-            { sabor: "Arepa solas", precio: "$1.500 pesos" },
-            { sabor: "Arepa con Queso Lonchis", precio: "$3.000 pesos" },
-            { sabor: "Arepa con Queso Cuajada", precio: "$3.000 pesos" },
-            { sabor: "Arepa con jamon y queso", precio: "$3.500 pesos" },
-            { sabor: "Arepa con pericos", precio: "$4.500 pesos" },
-            { sabor: "Arepa con chorizo", precio: "$6.000 pesos" },
-            { sabor: "Chorizo solo", precio: "$5.500 pesos" },
-            { sabor: "Arepa con pollo desmechado", precio: "$7.000 pesos" },
-            { sabor: "Arepa con carne desmechada", precio: "$7.000 pesos" },
-            { sabor: "Arepa con todo", precio: "$10.000 pesos" },
-            { sabor: "Arepa mixta pollo y carne", precio: "$8.000 pesos" }
-        ]
-    }
+  {
+    title: "De Lunes a viernes, Domingos y festivos",
+
+    image: "img/arepa_icono.png",
+    items: [
+      { sabor: "Arepa sin asar", precio: "$ 1.000 pesos" },
+      { sabor: "Arepa pequeña", precio: "$ 400 pesos" },
+      { sabor: "Arepa solas", precio: "$1.500 pesos" },
+      { sabor: "Arepa con Queso Lonchis", precio: "$3.500 pesos" },
+      { sabor: "Arepa con Queso Cuajada", precio: "$3.500 pesos" },
+      { sabor: "Arepa con jamón y queso", precio: "$4.000 pesos" },
+      { sabor: "Arepa con chorizo", precio: "$6.000 pesos" },
+      { sabor: "Chorizo solo", precio: "$5.000 pesos" },
+      { sabor: "Arepa con huevos revueltos", precio: "$3.500 pesos" },
+      { sabor: "Arepa con pericos", precio: "$4.500 pesos" },
+      { sabor: "Arepa con chorizo", precio: "$6.000 pesos" },
+      { sabor: "Arepa con carne hamburguesa", precio: "$5.500 pesos" },
+      { sabor: "Arepa con pollo desmechado", precio: "$8.000 pesos" },
+      { sabor: "Arepa con carne desmechada", precio: "$8.000 pesos" },
+      { sabor: "Arepa mixta pollo y carne", precio: "$9.000 pesos" },
+      { sabor: "Arepa con todo", precio: "$12.000 pesos" }
+    ]
+  },
+  {
+    title: "Bebidas",
+    image: "img/arepa_icono.png",
+    items: [
+      { sabor: "Cafe", precio: "$ 1.000 pesos" },
+      { sabor: "Cafe en leche", precio: "$ 2.000 pesos" },
+      { sabor: "Vaso de gaseosa", precio: "$ 1.000 pesos" } 
+    ]
+  }
 ];
 
 //Obtiene el contenedor del menu El DOM
